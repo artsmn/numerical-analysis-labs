90977d44be46096cb246ba84024a239c7d65479d
Artem Oliinyk <artem.oleynik.main@gmail.com>
2019-12-12 00:49:30 +0200

    added chord method

--- chmethods/chord.py
+++ chmethods/chord.py
@@ -1,3 +1,27 @@
 
 class Chord:
-    pass
+
+    def __init__(self, a, b, func, epsilon, d1f, d2f):
+        self.a = a
+        self.b = b
+        self.func = func
+        self.epsilon = epsilon
+        self.d1f = d1f
+        self.d2f = d2f
+        self.root = 0
+
+    def find_root(self):
+        if self.d1f(self.a)*self.d2f(self.a) > 0:
+            x0 = self.a
+            c = self.b
+        else:
+            x0 = self.b
+            c = self.a
+        xp = x0
+        while True:
+            xn = xp - self.func(xp)*(c-xp)/(self.func(c)-self.func(xp))
+            r = xn - xp
+            xp = xn
+            if abs(r) <= r:
+                break
+        self.root = xp
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import numpy as np
-from chmethods import Dichotomy
+import sympy as sp
+from chmethods import Dichotomy, Chord
 
 epsilon = 0.0001
-function = lambda x: np.power(x, 5) + 8 * x + 13
+a = -2
+b = 2
 
-dich = Dichotomy(-2, 2, function, epsilon)
+
+def f(x):
+    return np.power(x, 5) + 8 * x + 13
+
+
+dich = Dichotomy(a, b, f, epsilon)
 dich.find_root()
-print("Root is {}".format(dich.root))
+print("Root by Dichotomy is {}".format(dich.root))
 dich.draw_table()
 dich.draw_graph()
 
+
+x = sp.symbols('x')
+d1f = sp.diff(np.power(x, 5) + 8 * x + 13)
+d1f = sp.lambdify(x, d1f)
+d2f = sp.diff(np.power(x, 5) + 8 * x + 13, x, 2)
+d2f = sp.lambdify(x, d2f)
+
+
+hord = Chord(a, b, f, epsilon, d1f, d2f)
+hord.find_root()
+print('Root by Chord is {}'.format(hord.root))
